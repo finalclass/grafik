2ea4bd83e858c8c44e945d1e4080b29e81d1d685
Szymon Wygnanski <s@finalclass.net>
2019-11-10 20:26:25 +0100

    -

--- app/release.sh
+++ app/release.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# export DATABASE_URL=ecto://postgres:awefiwr222@localhost/grafik
-export DATABASE_URL=ecto://postgres:e3i29rse20-r!@localhost/grafik
-export MIX_ENV=prod
-export SECRET_KEY_BASE=arse3erirsefruienrst82nert8ttrmarstienrfrmrfpierf22189rerst8rtein38yrstienr3y8nritsxenri3p8unristemxc.t,mory38pnriftsrt
+docker build --build-arg SECRET_KEY_BASE=$1 --build-arg DATABASE_URL=ecto://postgres:$2@localhost/postgres -t finalclass/grafik:latest .
 
-cd assets
-npm run deploy
-cd ../
+docker push finalclass/grafik:latest
 
-mix compile
-mix release
-
-docker build -t finalclass/grafik:latest .
-
-# docker build --build-arg SECRET_KEY_BASE=$1 --build-arg DATABASE_URL=ecto://postgres:$2@localhost/postgres -t finalclass/grafik:latest .
-
-# docker push finalclass/grafik:latest
-
-# ssh fc1 "fc-grafik sync && fc-grafik restart"
+ssh fc1 "fc-grafik sync && fc-grafik restart"
